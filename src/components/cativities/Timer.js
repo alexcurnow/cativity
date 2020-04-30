657f359472a789f43f1b08dbcc3217fd1c086683
Alex Curnow <alexcurnow@gmail.com>
2020-04-30 15:27:08 -0500

    Got timer working on a very basic level.

--- src/components/cativities/Timer.js
+++ src/components/cativities/Timer.js
@@ -1,24 +1,41 @@
-import React, { useContext, useRef, useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button } from '@material-ui/core'
 
 export const Timer = () => {
   const [seconds, setSeconds] = useState(0)
+  const [isRunning, setIsRunning] = useState(false)
+
+  useEffect(() => {
+    if (isRunning) {
+      const id = window.setInterval(() => {
+        setSeconds((seconds) => seconds + 1)
+      }, 1000)
+      return () => window.clearInterval(id)
+    }
+    return undefined
+  }, [isRunning])
 
   return (
-    <div className="timerCircle">
-      <div>{seconds}</div>
-      <Button
-        variant="contained"
-        color="secondary"
-        onClick={(e) => {
-          window.setInterval(() => {
-            setSeconds((seconds) => seconds + 1)
-          }, 1000)
-          e.target.disabled = true
-        }}
-      >
-        Start Cativity
-      </Button>
-    </div>
+    <>
+      <div className="timerCircle">
+        <div>{seconds}</div>
+      </div>
+      <div className="start-end-buttons">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setIsRunning(true)}
+        >
+          Start Cativity
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => setIsRunning(false)}
+        >
+          End Cativity
+        </Button>
+      </div>
+    </>
   )
 }
